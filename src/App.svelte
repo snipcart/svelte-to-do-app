6c87d54fbaf18314e32d09f70a53790852a867fc
Diego Salinas Gardón <41164596+Dsalinasgardon@users.noreply.github.com>
2022-02-28 17:19:18 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,104 @@
+<script>
+	  let newItem = "";
+  let todoList = [];
+
+  function addTodo() {
+    if (newItem !== "") {
+      todoList = [
+        ...todoList,
+        {
+          task: newItem,
+          completed: false,
+        },
+      ];
+      newItem = "";
+    }
+
+    console.log(todoList);
+  }
+</script>
+
+<main class="container">
+	<main class="container">
+		<div>
+		  <form on:submit|preventDefault={addTodo}>
+			<input
+			  bind:value={newItem}
+			  type="task"
+			  class="todos__input"
+			  placeholder="Enter Todo"
+			/>
+			<button class="todos__button">+</button>
+		  </form>
+		  <h2 class="todos__listHeader">My to-do list</h2>
+		</div>
+		{#each todoList as item, index}
+		<div class="todo">
+		  <span class="todo__text">{item.task}</span>
+		</div>
+	  {/each}
+	  </main>
+
+</main>
+
+<style>
+ .todos__listHeader {
+    text-align: center;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 0 15px rgb(0 0 0 / 20%);
+    background-color: hsla(0, 0%, 100%, 0.2);
+    -webkit-backdrop-filter: blur(25px);
+    backdrop-filter: blur(25px);
+    margin: 15px 0px 25px 0px;
+    font-size: 1.2rem;
+  }
+	.todo {
+    display: flex;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0 0 15px rgb(0 0 0 / 20%);
+    background-color: hsla(0, 0%, 100%, 0.2);
+    -webkit-backdrop-filter: blur(25px);
+    backdrop-filter: blur(25px);
+    width: inherit;
+    margin-top: 15px;
+    font-size: 1.2rem;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 90vh;
+    background: #222e50
+      url(https://images.unsplash.com/photo-1631398741662-3e0a047c6c5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)
+      no-repeat;
+    background-size: cover;
+    padding-top: 10vh;
+  }
+
+  .todos__input {
+    background-color: inherit;
+    border: none;
+    box-shadow: none;
+    text-decoration: none;
+    font-size: 1.2rem;
+    border-bottom: 1px solid black;
+    margin-top: 15px;
+    outline: none;
+    width: 500px;
+  }
+  .todos__button {
+    background-color: inherit;
+    border: none;
+    box-shadow: none;
+    font-size: 1.2rem;
+    cursor: pointer;
+
+  }
+  
+</style>
---
+++ src/Icons.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let name;
+    export let width = "1.5rem";
+    export let height = "1.5rem";
+    export let focusable = false;
+    let icons = [
+      {
+        box: 24,
+        name: "check-mark",
+        svg: `<svg focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.77 4.93l1.4 1.4L8.43 19.07l-5.6-5.6 1.4-1.4 4.2 4.2L19.77 4.93m0-2.83L8.43 13.44l-4.2-4.2L0 13.47l8.43 8.43L24 6.33 19.77 2.1z"></path></svg>`,
+      },
+      {
+        box: 32,
+        name: "delete",
+        svg: `<svg focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z"></path></svg>`,
+      },
+    ];
+    let displayIcon = icons.find((e) => e.name === name);
+  </script>
+  
+  <svg
+    class={$$props.class}
+    {focusable}
+    {width}
+    {height}
+    viewBox="0 0 {displayIcon.box} {displayIcon.box}">{@html displayIcon.svg}</svg
+  >
